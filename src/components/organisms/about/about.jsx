7abae8866ec3250bbab5c432ca941526e4dc4760
totorototo <el.totorototo@gmail.com>
2023-10-05 16:27:00 +0200

    fix style for mobile device

--- src/components/organisms/about/about.jsx
+++ src/components/organisms/about/about.jsx
@@ -2,7 +2,7 @@ import { Flex, Text } from "../../elements/index.js";
 
 export const About = ({ about }) => {
   return (
-    <Flex mb={[3]} flexDirection={["column"]}>
+    <Flex mt={[3, 0]} mb={[3]} flexDirection={["column"]}>
       {about.map((statement, index) => (
         <Text mb={[3]} fontSize={[2]} color={"text.secondary"} key={index}>
           {statement}
