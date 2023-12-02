39f1659a0a8060725135c1def35c8b3a1a00fba6
toto rototo <el.totorototo@gmail.com>
2023-12-02 10:00:28 +0100

    add 3d scene

---
+++ src/components/misc/macintosh/Macintosh.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/apple_macintosh/scene.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh
+          geometry={nodes.Object_2.geometry}
+          material={materials["02___Default"]}
+        />
+        {/*       <mesh
+          geometry={nodes.Object_3.geometry}
+          material={materials.Background}
+        />*/}
+        <mesh
+          geometry={nodes.Object_4.geometry}
+          material={materials.dark_beige}
+        />
+        <mesh
+          geometry={nodes.Object_5.geometry}
+          material={materials.dark_grey}
+        />
+        <mesh
+          geometry={nodes.Object_6.geometry}
+          material={materials.darker_beige}
+        />
+        <mesh
+          geometry={nodes.Object_7.geometry}
+          material={materials.darker_beige}
+        />
+        <mesh
+          geometry={nodes.Object_8.geometry}
+          material={materials.darkest_beige}
+        />
+        <mesh
+          geometry={nodes.Object_9.geometry}
+          material={materials.darkest_beige}
+        />
+        <mesh
+          geometry={nodes.Object_10.geometry}
+          material={materials.light_beige}
+        />
+        <mesh
+          geometry={nodes.Object_11.geometry}
+          material={materials.lighter_beige}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/apple_macintosh/scene.glt");
