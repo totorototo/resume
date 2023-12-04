27d5e4b5db79cb0771115a05eee8a432b558b53e
TotoRoToto <el.totorototo@gmail.com>
2023-12-04 15:58:26 +0100

    update side project template (#18)

--- src/components/organisms/projects/projects.jsx
+++ src/components/organisms/projects/projects.jsx
@@ -48,20 +48,23 @@ export const Projects = ({ propjects }) => {
       {propjects.map((project, index) => (
         <Flex flexDirection={["column"]} mb={[3]} key={index}>
           <Flex flexDirection={"row"} alignItems={"flex-start"}>
-            <Image
-              alt={"project logo"}
-              mt={[1]}
-              maxWidth={64}
-              maxHeight={64}
-              ratio={16 / 9}
-              src={getImageUrl(project.name)}
-            />
+            <Link href={project.url}>
+              <Image
+                alt={"project logo"}
+                mt={[1]}
+                maxWidth={64}
+                maxHeight={64}
+                ratio={16 / 9}
+                src={getImageUrl(project.name)}
+              />
+            </Link>
+
             <Flex ml={[3]} flexDirection={"column"}>
               <Text mb={[1]} fontSize={[2]} color={"text.primary"}>
                 {`${project.name}`}
               </Text>
               <AnimatedText
-                mb={[1]}
+                mb={[3]}
                 fontSize={[2]}
                 color={
                   project.stack.includes(selectedItem)
@@ -71,9 +74,6 @@ export const Projects = ({ propjects }) => {
               >
                 {project.description}
               </AnimatedText>
-              <Link href={project.url} variant="nav1" mb={[2]}>
-                <Text color={"text.secondary"}>{project.url}</Text>
-              </Link>
               <Flex ref={ref} flexWrap={"wrap"}>
                 {project.stack.map((item, index) => (
                   <Pill
